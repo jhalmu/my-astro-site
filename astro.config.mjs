dd08247140efaade619118709699873100f6ef79
Juha Halmu <jhalmu@users.noreply.github.com>
2022-09-02 01:31:20 +0300

    Initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,15 @@
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+
+    site: 'https://stackoverlove.me',
+    sitemap: true,
+},
+    {
+        vite: {
+            ssr: {
+                external: ['svgo'],
+            }
+        }
+    });
